e5c7579808887f384130a53730275211ab048fb1
Blake Kotter <116109759+blakekotter@users.noreply.github.com>
2024-09-06 19:18:37 -0600

    console.log(`marquserid: ${marquserId}`)

--- src/app/extensions/DisplayIframe.jsx
+++ src/app/extensions/DisplayIframe.jsx
@@ -686,14 +686,14 @@ const deleteRecord = async (recordId, objectType) => {
       const userid = context.user.id;
       const clientid = 'wfcWQOnE4lEpKqjjML2IEHsxUqClm6JCij6QEXGa';
       const clientsecret = 'YiO9bZG7k1SY-TImMZQUsEmR8mISUdww2a1nBuAIWDC3PQIOgQ9Q44xM16x2tGd_cAQGtrtGx4e7sKJ0NFVX';
-      const marquserId = marquserid; // Assuming user ID is in context
+      const marquserId = marquserid; // This is from the global marquserid
       const marqaccountid = "163559625"; 
       const recordid = context.crm?.objectId || ''; // Assuming CRM record ID is in context
       const templateid = template?.id || ''; // Fetching template ID from the clicked template
       const templatetitle = template?.title || ''; // Fetching template title from the clicked template
-
+      console.log(`marquserid: ${marquserId}`)
       const objectType = context.crm?.objectType || '';
-      
+
       console.log("Collected parameters:", { refresh_token, clientid, clientsecret, marquserId, recordid, templateid, templatetitle, marqaccountid, dataSetId });
 
       const dynamicValue = (configData.value && context.crm.properties && configData.value in context.crm.properties)
