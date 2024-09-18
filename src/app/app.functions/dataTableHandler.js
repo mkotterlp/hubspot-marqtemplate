49b9329cc0b1000790afb182d6f3a460f73e6bed
Blake Kotter <116109759+blakekotter@users.noreply.github.com>
2024-09-18 13:47:27 -0600

    This should make it so the marq account data doesnt go null when the handleClick function is called

--- src/app/app.functions/dataTableHandler.js
+++ src/app/app.functions/dataTableHandler.js
@@ -17,7 +17,7 @@ exports.main = async (context) => {
         let marqembedTable = tablesResponse.results.find(table => table.name.toLowerCase() === 'marq_account_data');
 
         if (!marqembedTable) {
-            console.log('Table marqembed2 not found, creating new table');
+            console.log('Table marq_account_data not found, creating new table');
             const tableCreationResponse = await hubspotClient.cms.hubdb.tablesApi.createTable({
                 name: 'marq_account_data',
                 label: 'Marq Account Data',
@@ -66,14 +66,18 @@ exports.main = async (context) => {
 
             if (existingRow) {
                 console.log(`Updating existing row for object type: ${objectType}`);
-                // Update the existing row with empty values initially
+                
+                // Only update fields if they are empty or missing
+                const updatedValues = {
+                    accountId: existingRow.values.accountId || "",  // Keep existing value if not empty
+                    refreshToken: existingRow.values.refreshToken || "",  // Keep existing value if not empty
+                    datasetid: existingRow.values.datasetid || "",  // Keep existing value if not empty
+                    collectionid: existingRow.values.collectionid || ""  // Keep existing value if not empty
+                };
+
+                // Update the row if necessary
                 await hubspotClient.cms.hubdb.rowsApi.updateDraftTableRow(tableId, existingRow.id, {
-                    values: {
-                        accountId: "", 
-                        refreshToken: "", 
-                        datasetid: "", 
-                        collectionid: "", 
-                    }
+                    values: updatedValues
                 });
             } else {
                 console.log(`Creating new row for object type: ${objectType}`);
@@ -127,130 +131,3 @@ exports.main = async (context) => {
         };
     }
 };
-
-
-
-
-
-
-
-
-
-// const hubspot = require('@hubspot/api-client');
-
-// exports.main = async (context) => {
-//     const hubspotClient = new hubspot.Client({
-//         accessToken: process.env['PRIVATE_APP_ACCESS_TOKEN'],
-//     });
-
-//     let tableId;
-
-//     try {
-//         // Fetch all tables
-//         const tablesResponse = await hubspotClient.cms.hubdb.tablesApi.getAllTables();
-//         if (!tablesResponse || !tablesResponse.results) {
-//             throw new Error('Failed to fetch tables');
-//         }
-
-//         let marqembedTable = tablesResponse.results.find(table => table.name.toLowerCase() === 'marq_account_data');
-
-//         if (!marqembedTable) {
-//             console.log('Table marqembed2 not found, creating new table');
-//             const tableCreationResponse = await hubspotClient.cms.hubdb.tablesApi.createTable({
-//                 name: 'marq_account_data',
-//                 label: 'Marq Account Data',
-//                 columns: [
-//                     { name: 'objectType', label: 'Object Type', type: 'TEXT' },
-//                     { name: 'accountId', label: 'Account ID', type: 'TEXT' },
-//                     { name: 'refreshToken', label: 'Refresh token', type: 'TEXT' },
-//                     { name: 'datasetid', label: 'Data set ID', type: 'TEXT' },
-//                     { name: 'collectionid', label: 'Collection ID', type: 'TEXT' },
-                    
-//                 ],
-//             });
-//             tableId = tableCreationResponse.id;
-//             marqembedTable = tableCreationResponse;
-
-//             console.log('Table account created and published');
-//         } else {
-//             tableId = marqembedTable.id;
-//             console.log('Table account found with ID:', tableId);
-//         }
-
-//         console.log('Fetching rows from table');
-//         let rowsResponse = await hubspotClient.cms.hubdb.rowsApi.getTableRows(tableId);
-//         if (!rowsResponse || !rowsResponse.results) {
-//             throw new Error('Failed to fetch rows from the table');
-//         }
-
-//         // Fetching all CRM object types
-//         const objectTypesResponse = await hubspotClient.crm.schemas.coreApi.getAll();
-//         if (!objectTypesResponse || !objectTypesResponse.results) {
-//             throw new Error('Failed to fetch CRM object types');
-//         }
-//         const customObjectTypes = objectTypesResponse.results.map(obj => obj.name.toLowerCase());
-
-//         // Including standard object types
-//         const standardObjectTypes = ['contact', 'company', 'deal', 'ticket', 'data'];
-
-//         const allObjectTypes = [...standardObjectTypes.map(type => type.toLowerCase()), ...customObjectTypes];
-
-//         let existingObjectTypes = rowsResponse.results
-//             .filter(row => row.values.objectType) // Filter out rows without objectType
-//             .map(row => row.values.objectType.toLowerCase());
-
-
-//         for (const objectType of allObjectTypes) {
-//             if (!existingObjectTypes.includes(objectType.toLowerCase())) {
-//                 console.log(`Creating new row for object type: ${objectType}`);
-//                 const rowValues = {
-//                     objectType: objectType,
-//                     accountId: "", 
-//                     refreshToken: "", 
-//                     datasetid: "", 
-//                     collectionid: "", 
-//                 };
-//                 await hubspotClient.cms.hubdb.rowsApi.createTableRow(tableId, { values: rowValues });
-//             }
-//         }
-
-//         await hubspotClient.cms.hubdb.tablesApi.publishDraftTable(tableId);
-//         console.log('Table marqembed2 published after creating new rows.');
-
-//         // Re-fetch rows after publishing to ensure newly added rows are included
-//         rowsResponse = await hubspotClient.cms.hubdb.rowsApi.getTableRows(tableId);
-//         if (!rowsResponse || !rowsResponse.results) {
-//             throw new Error('Failed to fetch rows from the table after publishing');
-//         }
-
-//         // Assuming context.parameters contains necessary keys for filtering config
-//         const objectType = context.parameters?.objectType?.toLowerCase() || 'default'; // Default object type if not provided
-//         let matchedRow = rowsResponse.results.find(row => row.values.objectType && row.values.objectType.toLowerCase() === objectType);
-//         let dataRow = rowsResponse.results.find(row => row.values.objectType && row.values.objectType.toLowerCase() === 'data');
-
-
-//         if (!matchedRow && !dataRow) {
-//             throw new Error(`No configuration found for object type: ${objectType} or 'data'`);
-//         }
-
-//         return {
-//             body: JSON.stringify({
-//                 objectTypeRow: matchedRow || null,
-//                 dataRow: dataRow || null
-//             }),
-//             statusCode: 200,
-//         };
-
-//     } catch (error) {
-//         console.error('Error:', {
-//             message: error.message,
-//             statusCode: error.statusCode,
-//             body: error.body,
-//             stack: error.stack,
-//         });
-//         return {
-//             body: JSON.stringify({ error: 'An error occurred while processing your request.', details: error.message }),
-//             statusCode: 500,
-//         };
-//     }
-// };
